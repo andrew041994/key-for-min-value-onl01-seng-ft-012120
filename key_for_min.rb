9b6074f7ba823310f0525a22e2b5edf5b4f27686
Alehandro Persaud <andrew041994@gmail.com>
2020-01-25 04:44:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,16 @@
 
 def key_for_min_value(name_hash)
   if name_hash = {}
-    nil
+   return nil
  else
      min_value = name_hash.first[1]
      min_key = name_hash.first[0]
-     name_hash.collect do |key, value|
+     name_hash.each do |key, value|
        if value < min_value
          min_value = value
          min_key = key
        end
      end
- return min_key
+     min_key
  end
-
 end
